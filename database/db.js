526b70630289463fd71affacd28164c98cff2e26
kattiel <gabrielcaetano@unipam.edu.br>
2022-11-26 01:01:45 -0300

    first commit

--- database/db.js
+++ database/db.js
@@ -1,9 +1,9 @@
 import { Sequelize } from "sequelize"; // importar o sequelize
 
 const dbName = 'AgroTech';
-const dbUser = 'LucasUser';
+const dbUser = 'KattielUser';
 const dbHost = 'localhost';
-const dbPassword = '0147';
+const dbPassword = '123456';
 
 
 const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
